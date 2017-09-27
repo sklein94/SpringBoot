5a38cbe3dd5c65b3d01d0eb62dcc9ecaf25f00cc
sklein <simon.klein.sz.1@gmail.com>
2017-09-27 14:40:44 +0200

    Stand Anfang von: https://www.youtube.com/watch?v=yGeI_WORmco

--- src/main/java/test/test2/TopicService.java
+++ src/main/java/test/test2/TopicService.java
@@ -14,15 +14,29 @@ public class TopicService {
             new Topic("javascript", "JavaScript", "JavaScrip Description")
     ));
 
-    public List<Topic> getAllTopics(){
+    public List<Topic> getAllTopics() {
         return topics;
     }
 
-    public Topic getTopic(String id){
+    public Topic getTopic(String id) {
         return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
     }
 
     public void addTopic(Topic topic) {
         topics.add(topic);
+    }
+
+    public void updateTopic(String id, Topic topic) {
+        for (int i = 0; i < topics.size(); i++) {
+            Topic t = topics.get(i);
+            if (t.getId().equals(id)) {
+                topics.set(i, topic);
+                return;
+            }
+        }
+    }
+
+    public void deleteTopic(String id) {
+        topics.removeIf(t -> t.getId().equals(id));
     }
 }
